b5dd820aad8574e9e836db1282d7343e75231526
surya9813 <suryanarayan2056@gmail.com>
2024-07-01 08:48:49 +0545

    first commit

---
+++ Class_1/index.js
@@ -0,0 +1,123 @@
+
+
+// let email = "suryanarayan123@gmail.com";
+// let password = "999999999";
+
+// if(email === "suryanarayan123@gmail.com" && password === "9999999998"){
+//     console.log("Login successfully");
+// }else{
+//     console.log("Login unsuccessfully");
+// }
+
+// SEE result check
+
+// let grade = 85;
+
+// if(grade >=90){
+//     console.log("Grade:A+")
+// }else if(grade >=80){
+//     console.log("Grade:A")
+// }else if(grade >=70){
+//     console.log("Grade:B+")
+// }else if(grade >=60){
+//     console.log("Grade:B")
+// }else if(grade >=50){
+//     console.log("Grade:C+")
+// }else if(grade >=40){
+//     console.log("Grade:C")
+// }else{
+//     console.log("Failed")
+// }
+
+
+// Weather
+
+// let weather = "rainy";
+// if(weather === "sunny"){
+//     console.log("Today is a sunny day");
+// }else if(weather === "winter"){
+//     console.log("Today is a winter day");
+// }else if(weather === "cloudy"){
+//     console.log("Today is a cloudy day");
+// }else if(weather === "rainy"){
+//     console.log("Today is a rainy day");
+// }else{
+//     console.log("Invalid Weather");
+// }
+
+
+
+//Switch
+
+// let role = "super admin";
+// switch (role) {
+//     case "buyer":
+//         console.log("You can only buy product");
+//         break;
+//     case "seller":
+//         console.log("You can only sell product");
+//         break;
+//     case "admin":
+//         console.log("You can only add product");
+//         break;
+//     case "super admin":
+//         console.log("You can acess everythings");
+//         break;
+//     default:
+//         console.log("Invalid Role");
+// }
+
+
+
+
+
+// let  date = new Date().getDay;
+// console.log(date);
+
+// let  date = new Date()
+// console.log(date);
+
+
+// switch(date){
+//     case "0":
+//         console.log("Today is sunday");
+//         break;
+//     case "1":
+//         console.log("Today is monday");
+//         break;
+//     case "2":
+//         console.log("Today is tuesday");
+//         break;
+//     case "3":
+//         console.log("Today is wednesday");
+//         break;  
+//     default:
+//          console.log("Invalid Role");  
+// }
+
+
+//status
+
+let status = 200;
+switch (status) {
+    case "201":
+        console.log("created");
+        break;
+    case "400":
+        console.log("bad request");
+        break;
+    case "401":
+        console.log("UnAuthorized");
+        break;
+    case "403":
+        console.log("ForBidden");
+        break;
+    // case "501":
+    //     console.log("UnAuthorized");
+    //     break;
+    // case "403":
+    //     console.log("ForBidden");
+    //     break;
+    default:
+        console.log("Invalid Role");
+}
